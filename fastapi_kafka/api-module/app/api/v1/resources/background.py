85d0da31acb432d67381edca3a7912e9e90d44a3
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2022-09-30 10:19:04 +0700

    test kakfa producer & consumer

--- fastapi_kafka/api-module/app/api/v1/resources/background.py
+++ fastapi_kafka/api-module/app/api/v1/resources/background.py
@@ -4,15 +4,16 @@ import config
 from main_kafka import redis_object, producer
 
 
-async def process_background(task_id: str,
-                       data: DlResult
-                       ):
+async def process_background(
+        task_id: str,
+        data: DlResult
+):
 
     try:
         data_dump = json.dumps(data.__dict__)
         redis_object.set(task_id, data_dump)
         # print(config.ML_QUERY_NAME, config.ML_OBJECT_DETECTION_TASK)
-        await producer.send_and_wait(config.KAFKA_TOPIC, data_dump)
+        await producer.send_and_wait(config.KAFKA_TOPIC, json.dumps(data.__dict__, ensure_ascii=False).encode('utf-8'))
 
     except Exception as e:
         data.status = "FAILED"
--- fastapi_kafka/kafka/consumer_test.py
+++ fastapi_kafka/kafka/consumer_test.py
@@ -1,20 +1,18 @@
 from aiokafka import AIOKafkaConsumer
-
 import asyncio
-
 
 async def consume():
     consumer = AIOKafkaConsumer(
-        'requests',
+        'my_topic',
         bootstrap_servers='localhost:9092',
-        group_id="requests_consumers")
+        group_id="my-topic")
     # Get cluster layout and join group `my-group`
     await consumer.start()
     try:
         # Consume messages
         async for msg in consumer:
             print("consumed: ", msg.topic, msg.partition, msg.offset,
-                  msg.key, msg.value.decode(), msg.timestamp)
+                  msg.key, msg.value, msg.timestamp)
     finally:
         # Will leave consumer group; perform autocommit if enabled.
         await consumer.stop()
